da8aa8a542196aa9b097219e333f14a346dec81b
browloga490 <logoswego@gmail.com>
2019-07-16 10:18:52 -0400

    Added the 'Add Song' Page - basic functionality.

--- aggrobeats/src/App.js
+++ aggrobeats/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
 import Confirmation from "./Components/Confirmation";
+import AddSong from "./Components/AddSong";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Route exact path="/" component={Login} />
           <Route path="/home" component={Home} />
           <Route path="/confirmation" component={Confirmation} />
+          <Route path="/addsong" component={AddSong} />
         </div>
       </Router>
     );
--- aggrobeats/src/Components/AddSong.js
+++ aggrobeats/src/Components/AddSong.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Header, Button } from "semantic-ui-react";
 
-export default class Home extends Component {
+export default class AddSong extends Component {
   render() {
     return (
       <div>
-        <Header as="h1">Home</Header>
-        <Link to="/addsong">
-          <Button>Click Here</Button>
+        <Header as="h1">Admisson Info</Header>
+        <Link to="/confirmation">
+          <Button>Add New Song</Button>
         </Link>
       </div>
     );
