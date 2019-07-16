0349c84da2e4cac5013fea2f80aee4e88fbee0f9
Rizvi <rizvida1@gmail.com>
2019-07-16 18:31:49 -0400

    Modified addsong

--- aggrobeats/src/Components/AddSong.js
+++ aggrobeats/src/Components/AddSong.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Button, Form, Grid, Progress } from "semantic-ui-react";
+import { Button, Form, Grid, Progress, Header } from "semantic-ui-react";
 import Navbar from "./Navbar";
 import back from "../assets/backAbB.png";
 
@@ -8,18 +8,6 @@ export default class AddSong extends Component {
   state = { progress: 0 };
 
   handleChange = (e, { value }) => this.setState({ value });
-
-  handleUpload = () => {
-    this.setState({ progress: 100 });
-    // while (this.state.progress < 100) {
-    //   setTimeout(this.increment(), 10);
-    // }
-  };
-
-  //   increment = () => {
-  //     this.setState({ progress: this.state.progress + 10 });
-  //     console.log(this.state.progress);
-  //   };
 
   render() {
     const { value } = this.state;
@@ -31,7 +19,15 @@ export default class AddSong extends Component {
         }}
       >
         <Navbar history={this.props.history} />
-
+        <Grid centered>
+          <Header
+            as="h1"
+            inverted
+            style={{ background: "#0004", marginTop: 50, marginBottom: 50 }}
+          >
+            Add New Song
+          </Header>
+        </Grid>
         <Grid columns={2} verticalAlign="middle" centered>
           <Grid.Column>
             <Form inverted>
@@ -39,10 +35,13 @@ export default class AddSong extends Component {
                 <Form.Input
                   placeholder="Enter file name here"
                   label="Music File"
-                />{" "}
-                <Button onClick={() => this.handleUpload()}>Upload</Button>
+                  onChange={() => this.setState({ progress: 0 })}
+                />
+                <Button onClick={() => this.setState({ progress: 100 })}>
+                  Upload
+                </Button>
               </Form.Group>
-              <Progress percent={this.state.progress} indicating />
+              <Progress percent={this.state.progress} autoSuccess progress />
               <Form.Group inline>
                 <label>Upload Quality:</label>
                 <Form.Radio
